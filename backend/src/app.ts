a78c7dd607974649c1f0e0de060dada122a3b351
Adnan mahmood <140881151+msaadnanil18@users.noreply.github.com>
2024-08-11 20:03:12 +0000

    backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express, { urlencoded } from 'express';
+import mongoose from 'mongoose';
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import bcrypt from 'bcrypt';
+
+
+const app = express();
+
+
+export {app}
