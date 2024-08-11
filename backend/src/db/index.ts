fbf53b000c4d5d0be2dbc5a3912af7085f8e9886
Dev mode <adnan@exatorial.com>
2024-08-12 02:52:37 +0530

    prettier setup

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.HOST_DB_SRV}`,
+      `${process.env.HOST_DB_SRV}`
     );
     console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log('MONGODB connection FAILED ', error);
