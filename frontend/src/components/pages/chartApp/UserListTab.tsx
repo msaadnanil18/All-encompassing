43c45b1d0fd7ea9513eecb2949fa793fe7337bda
msaadnanil18 <msaandnil18@gmail.com>
2024-10-03 02:48:58 +0530

    fix(universal): ui fixes

--- frontend/src/components/pages/chartApp/UserListTab.tsx
+++ frontend/src/components/pages/chartApp/UserListTab.tsx
@@ -7,6 +7,7 @@ import { ChatListItemInterface } from '../../types/charts';
 const { useBreakpoint } = Grid;
 const { Sider } = Layout;
 import { useSearchParams } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const UserListTab: React.FC<{
   handelOnCreateChatSelect: (r: string) => Promise<void>;
@@ -37,71 +38,85 @@ const UserListTab: React.FC<{
 
   return (
     <Layout>
-      <Sider
-        theme="light"
-        width={455}
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          insetInlineStart: 0,
-          ...(isDark ? { backgroundColor: ' #171717' } : {}),
-          top: 0,
-          bottom: 0,
-          scrollbarWidth: 'thin',
-          scrollbarColor: 'auto',
-        }}
+      <motion.div
+        initial={{ x: '-100%' }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.6 }}
       >
-        <List
-          loading={chatListLoading}
-          header={
-            <UserListHeader
-              {...{
-                isDark,
-                handelOnCreateChatSelect,
-                isOpenSearchBar,
-                closeSearchBar,
-                openSearchBar,
-                searchOptions,
-                handelOnSearchChange,
-                searchTerm,
-              }}
-            />
-          }
-          itemLayout="horizontal"
-          dataSource={chatList}
-          renderItem={(chat, index) => {
-            const prevChats = chat.members.find((user) => user._id !== userId);
-            let _prevChats = { ...prevChats };
-            if (!prevChats?.avatar) {
-              _prevChats.avatar = `https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`;
-            }
-            return (
-              <List.Item
-                onClick={(e) => {
-                  setSearchParams({
-                    name: chat.name.split('-')[1],
-                    id: chat._id,
-                  });
-                }}
-                style={{ cursor: 'pointer', padding: '10px 15px' }}
-              >
-                <List.Item.Meta
-                  avatar={
-                    <Avatar
-                      size={50}
-                      src={_prevChats?.avatar}
-                      icon={<UserOutlined />}
-                    />
-                  }
-                  title={_prevChats?.name}
-                  description={_prevChats?.email}
-                />
-              </List.Item>
-            );
+        <Sider
+          theme="light"
+          width={455}
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            insetInlineStart: 0,
+            ...(isDark ? { backgroundColor: ' #171717' } : {}),
+            top: 0,
+            bottom: 0,
+            scrollbarWidth: 'thin',
+            scrollbarColor: 'auto',
           }}
-        />
-      </Sider>
+        >
+          <List
+            loading={chatListLoading}
+            header={
+              <UserListHeader
+                {...{
+                  isDark,
+                  handelOnCreateChatSelect,
+                  isOpenSearchBar,
+                  closeSearchBar,
+                  openSearchBar,
+                  searchOptions,
+                  handelOnSearchChange,
+                  searchTerm,
+                }}
+              />
+            }
+            itemLayout="horizontal"
+            dataSource={chatList}
+            renderItem={(chat, index) => {
+              const prevChats = chat.members.find(
+                (user) => user._id !== userId
+              );
+              let _prevChats = { ...prevChats };
+              if (!prevChats?.avatar) {
+                _prevChats.avatar = `https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`;
+              }
+              return (
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.9 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.4, delay: 0.2 }}
+                >
+                  <List.Item
+                    onClick={(e) => {
+                      setSearchParams({
+                        name: chat.name.split('-')[1],
+                        id: chat._id,
+                      });
+                    }}
+                    style={{ cursor: 'pointer', padding: '10px 15px' }}
+                  >
+                    <List.Item.Meta
+                      avatar={
+                        <Avatar
+                          size={50}
+                          src={_prevChats?.avatar}
+                          icon={<UserOutlined />}
+                        />
+                      }
+                      title={_prevChats?.name}
+                      description={_prevChats?.email}
+                    />
+                  </List.Item>
+                </motion.div>
+              );
+            }}
+          />
+        </Sider>
+      </motion.div>
     </Layout>
   );
 };
